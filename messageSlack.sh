59d65d88bd8fc85935f6a1b83691ee479713237f
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2019-06-21 00:15:34 +0900

    deploy only hosting and checked firebase serve works.

---
+++ messageSlack.sh
@@ -0,0 +1,2 @@
+curl -X POST -H 'Content-type: application/json' --data '{"text":"deploy to firebase hosting succeeded!"}' \
+  https://hooks.slack.com/services/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
