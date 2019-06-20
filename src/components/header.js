e3ec25f6db7d03bd6134220cecd40c9c9b78b3fa
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2019-06-20 20:01:06 +0900

    Initial commit

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+const Header = props => <h1>{props.headerText}</h1>
+Header.propTypes = {
+  headerText: PropTypes.string.isRequired
+};
+
+export default Header;
