b7ab28553939863198c0f9ce3ea19fc3ff1912a1
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2019-06-21 17:08:31 +0900

    Added typescript plugin.

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,14 @@ module.exports = {
   /* Your site config here */
   plugins: [
     'gatsby-plugin-emotion',
-    'gatsby-plugin-react-helmet'
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
   ],
-
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
-import React from "react"
+import * as React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+
+const title: string = 'hello';
 
 export default () => (
   <div style={{ color: `purple` }}>
@@ -9,7 +11,7 @@ export default () => (
     <p><Link to="/contact/">Contact</Link></p>
 
     <Header headerText="Hello Gatsby!" />
-    <p>What a world.</p>
+    <p>{title}</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
 )
